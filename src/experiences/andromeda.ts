db09b7cdcdad63b6855fda41a279fe5df12b5785
patudom <pudompra@cfa.harvard.edu>
2026-08-25 21:58:04 -0400

    Add remaining tour steps

--- src/experiences/andromeda.ts
+++ src/experiences/andromeda.ts
@@ -31,18 +31,46 @@ export const andromedaExperience: TourExperience = [
     instaText: "Hubble sees this...",
   },
   {
-    title: "PHAST Frames",
-    tourSheetText: [],
-    instaText: "",
+    title: "Over 1,000 Hubble images",
+    tourSheetText: [
+      "To obtain this view with Hubble, astronomers stitched together 1,400 images taken over 500 hours.",
+      "The overlaid grid shows where each individual image was taken across Andromeda to make this beautiful mosaic.",
+    ],
+    instaText: "Hubble took 1,400 images (and 500 hours)!",
   },
   {
-    title: "SF Disk",
-    tourSheetText: [],
-    instaText: "",
+    title: "Webb",
+    tourSheetText: [
+      "The James Webb Space Telescope takes images in infrared light",
+      "It can see a similar amount of sky at once as Hubble."
+    ],
+    instaText: "Webb can see only tiny pieces at a time",
   },
   {
-    title: "Roman",
-    tourSheetText: [],
-    instaText: "",
+    title: "Roman, Webb, and Hubble",
+    tourSheetText: [
+      "The Roman Space Telescope will take infrared images similar to Webb, but it can see MUCH more of the sky at once compared with Webb and Hubble.",
+    ],
+    instaText: "Roman covers way more sky per infrared image.",
+  },
+  {
+    title: "Roman's view of Andromeda",
+    tourSheetText: [
+      "Just 6 images (taken over 3 hrs) with Roman will cover more of Andromeda than 1,400 images (taken over 500 hrs) with Hubble!",
+    ],
+    instaText: "Roman is over 100x faster than Hubble.",
+  },
+  {
+    title: "Roman's tiny pixels",
+    tourSheetText: [
+      "Even with its huge field of view, Roman's high resolution will allow very detailed images, similar to Hubble.",
+    ],
+    instaText: "Roman images will have similar detail to Hubble.",
+  },
+  {
+    title: "Explore on your own",
+    tourSheetText: [
+      "You can continue exploring different regions of the sky and the fields of view of Hubble, Webb, and Roman.",
+    ],
   },
 ];
